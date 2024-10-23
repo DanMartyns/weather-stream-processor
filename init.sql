703545b606830e57c530fce8be4428fcaf02f333
Daniel Coelho <dmcoelho95@gmail.com>
2024-10-23 23:16:50 +0100

    Add comments and simplify code for clarity

--- init.sql
+++ init.sql
@@ -1,5 +1,7 @@
 CREATE TABLE weather_data (
     id SERIAL PRIMARY KEY,
-    timestamp TIMESTAMP,
-    total_precipitation DOUBLE PRECISION
+    timestamp TIMESTAMP NOT NULL,
+    lat DOUBLE PRECISION NOT NULL,  -- Latitude
+    lon DOUBLE PRECISION NOT NULL,  -- Longitude
+    hourly_precipitation DOUBLE PRECISION NOT NULL  -- Hourly precipitation in mm
 );
