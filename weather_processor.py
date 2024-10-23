46ef79c40756c4c43efae3ec8aa939ad359d62c0
Daniel Coelho <dmcoelho95@gmail.com>
2024-10-23 23:16:50 +0100

    [bugfix] Add comments and simplify code for clarity

--- weather_processor.py
+++ weather_processor.py
@@ -5,15 +5,13 @@ import asyncio
 import psycopg2
 from pyspark.sql import SparkSession
 from datetime import datetime
-from pyspark.sql.functions import from_json, col, sum, window, to_timestamp
+from pyspark.sql.functions import from_json, col, sum
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.StreamHandler(sys.stderr)  # Log to stderr to be captured by Docker
-    ],
+    handlers=[logging.StreamHandler(sys.stdout)],
 )
 
 log = logging.getLogger(__name__)
@@ -30,6 +28,27 @@ POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 async def wait_for_kafka_connection(bootstrap_servers, topic, retries=10, delay=2):
     """
     Asynchronously tries to establish a connection to Kafka using Spark with a retry mechanism.
+
+    Parameters:
+    ----------
+    bootstrap_servers : str
+        The Kafka bootstrap servers to connect to, formatted as "host:port".
+    topic : str
+        The Kafka topic to subscribe to for reading data.
+    retries : int, optional
+        The number of times to retry connecting to Kafka if the initial connection fails (default is 10).
+    delay : int, optional
+        The delay in seconds between retry attempts (default is 2 seconds).
+
+    Returns:
+    -------
+    pyspark.sql.dataframe.DataFrame
+        A DataFrame representing the Kafka stream if the connection is successful.
+
+    Raises:
+    -------
+    Exception
+        If the connection cannot be established after the specified number of retries.
     """
     for attempt in range(retries):
         try:
@@ -55,20 +74,51 @@ async def wait_for_kafka_connection(bootstrap_servers, topic, retries=10, delay=
             log.error(f"Error: {e}")
             await asyncio.sleep(delay)
 
+    raise Exception("Failed to connect to Kafka after multiple attempts.")
 
 async def read_kafka_data(kafka_df):
     """
     Processes the Kafka data stream with Spark.
+
+    This function reads the incoming Kafka data, parses it according to the specified schema,
+    aggregates the total precipitation by timestamp and location, and sets up a streaming query
+    to output the results to the console and store them in a PostgreSQL database.
+
+    Parameters:
+    ----------
+    kafka_df : pyspark.sql.dataframe.DataFrame
+        A Spark DataFrame representing the incoming Kafka stream. This DataFrame should contain
+        the raw data from Kafka, which includes JSON-encoded values.
+
+    Returns:
+    -------
+    None
+        This function does not return a value. It sets up a streaming query that runs indefinitely
+        until stopped.
+
+    Raises:
+    -------
+    Exception
+        Any exceptions that occur during the processing of the Kafka stream or while writing to
+        the database will be raised and logged.
     """
 
-    # Get the Spark session from the Kafka DataFrame
-    spark = kafka_df.sparkSession  
-
-    # Define schema for the Kafka stream data
+    # Specify the updated schema to match producer data
     schema = StructType(
         [
             StructField("timestamp", StringType(), True),
-            StructField("total_precipitation", DoubleType(), True),
+            StructField(
+                "total_precipitation", DoubleType(), True
+            ),  # Total precipitation
+            StructField(
+                "location",
+                StructType(
+                    [  # Location as nested struct
+                        StructField("lat", DoubleType(), True),  # Latitude
+                        StructField("lon", DoubleType(), True),  # Longitude
+                    ]
+                ),
+            ),
         ]
     )
 
@@ -76,81 +126,128 @@ async def read_kafka_data(kafka_df):
     weather_data = (
         kafka_df.selectExpr("CAST(value AS STRING)")
         .select(from_json(col("value"), schema).alias("data"))
-        .select("data.timestamp", "data.total_precipitation")
+        .select(
+            "data.timestamp",
+            "data.total_precipitation",
+            "data.location.lat",
+            "data.location.lon",
+        )
     )
 
-    # Write the stream to the database
-    weather_data.writeStream.outputMode("update").foreachBatch(
-        store_weather_data
-    ).start().awaitTermination()
+    # Group by Timestamp and Location and SUM the precipitation for each hour
+    precipitation_summary = weather_data.groupBy("timestamp", "lat", "lon").agg(
+        sum("total_precipitation").alias("hourly_precipitation")
+    )
 
-    # Process the data (e.g., convert timestamp, apply watermark)
-    processed_data = weather_data.select(
-        to_timestamp("timestamp", "yyyy-MM-dd HH:mm:ss").alias("timestamp"),
-        "total_precipitation",
-    ).withWatermark("timestamp", "1 hour")
-
-    # Create a temporary view for your streaming DataFrame
-    weather_data.createOrReplaceTempView("weather_data")
-
-    # Define and execute the SQL query to aggregate precipitation
-    precipitation_summary = spark.sql("""
-        SELECT 
-            window(timestamp, '1 hour') AS window,
-            SUM(total_precipitation) AS hourly_precipitation
-        FROM 
-            weather_data
-        GROUP BY 
-            window(timestamp, '1 hour')
-        ORDER BY 
-            window
-    """)
-
-    # To write the results to the console or any other sink
-    query = precipitation_summary.write \
-        .outputMode("update") \
-        .format("console") \
+    # Display the result to console or store in the database
+    query = (
+        precipitation_summary.writeStream.outputMode("update")
+        .format("console")
+        .foreachBatch(store_weather_data)
         .start()
+    )
 
     query.awaitTermination()
 
 
-def get_db_connection():
-    try:
-        return psycopg2.connect(
-            host="postgres",
-            database=POSTGRES_DB,
-            user=POSTGRES_USER,
-            password=POSTGRES_PASSWORD,
-        )
-    except psycopg2.OperationalError as e:
-        log.error(f"PostgreSQL connection error: {e}")
-        raise
+def get_db_connection(retries=5, delay=5):
+    """
+    Establishes a connection to the PostgreSQL database with retry mechanism.
+
+    This function attempts to connect to the PostgreSQL database specified by the 
+    global connection parameters. If the connection fails, it will retry the specified 
+    number of times with a delay between attempts.
+
+    Parameters:
+    ----------
+    retries : int, optional
+        The number of times to retry connecting to the database. Default is 5.
+    
+    delay : int, optional
+        The number of seconds to wait between retries. Default is 5.
+
+    Returns:
+    -------
+    connection : psycopg2.extensions.connection
+        A connection object to the PostgreSQL database if the connection is successful.
+
+    Raises:
+    -------
+    Exception
+        Raises an exception if the connection fails after the specified number of retries.
+    """
+    for attempt in range(retries):
+        try:
+            return psycopg2.connect(
+                host="postgres",
+                database=POSTGRES_DB,
+                user=POSTGRES_USER,
+                password=POSTGRES_PASSWORD,
+            )
+        except psycopg2.OperationalError as e:
+            log.error(f"PostgreSQL connection error (attempt {attempt + 1}/{retries}): {e}")
+            time.sleep(delay)
+    raise Exception("Failed to connect to PostgreSQL after multiple attempts.")
 
 
 def store_weather_data(batch_df, batch_id):
     """
     Stores the fetched weather data into the PostgreSQL database from a Spark DataFrame.
+
+    This function takes a batch of weather data, filters out any rows with 
+    missing values for key fields, and inserts the valid data into the 
+    PostgreSQL database. Each insertion is logged for tracking purposes.
+
+    Parameters:
+    ----------
+    batch_df : pyspark.sql.DataFrame
+        A Spark DataFrame containing the weather data to be stored. It should include 
+        columns for timestamp, latitude, longitude, and hourly precipitation.
+    
+    batch_id : int
+        The identifier for the batch of data being processed. This is primarily 
+        for logging purposes and can be used to track which batch is being stored.
+
+    Returns:
+    -------
+    None
+
+    Raises:
+    -------
+    Exception
+        Raises an exception if there is an error during the database connection 
+        or data insertion process.
     """
     conn = None
     try:
         conn = get_db_connection()
         with conn:
             with conn.cursor() as cursor:
-                # Filter out rows with None values for timestamp or total_precipitation
-                filtered_df = batch_df.filter(col("timestamp").isNotNull() & col("total_precipitation").isNotNull())
-                
+                # Filter out rows with None values for timestamp or precipitation
+                filtered_df = batch_df.filter(
+                    col("timestamp").isNotNull()
+                    & col("hourly_precipitation").isNotNull()
+                    & col("lat").isNotNull()
+                    & col("lon").isNotNull()
+                )
+
                 # Iterate over the rows of the DataFrame
                 for row in filtered_df.collect():
                     cursor.execute(
                         """
-                        INSERT INTO weather_data (timestamp, total_precipitation)
-                        VALUES (%s, %s);
+                        INSERT INTO weather_data (timestamp, lat, lon, hourly_precipitation)
+                        VALUES (%s, %s, %s, %s);
                         """,
-                        (datetime.fromtimestamp(int(row.timestamp)), row.total_precipitation),
+                        (
+                            datetime.fromtimestamp(int(row.timestamp)),
+                            row.lat,
+                            row.lon,
+                            row.hourly_precipitation,
+                        ),
                     )
                     log.info(
-                        f"Stored data in database: {datetime.fromtimestamp(int(row.timestamp))} | Precipitation: {row.total_precipitation} mm"
+                        f"Stored data in database: {datetime.fromtimestamp(int(row.timestamp))} | "
+                        f"Location: ({row.lat}, {row.lon}) | Precipitation: {row.hourly_precipitation} mm"
                     )
     except Exception as e:
         log.error(f"Error storing data in database: {e}")
--- weather_stream.py
+++ weather_stream.py
@@ -6,10 +6,9 @@ import json
 from kafka import KafkaProducer
 from datetime import datetime
 import asyncio
-from kafka.errors import NoBrokersAvailable
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARN,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[logging.StreamHandler(sys.stdout)],
 )
@@ -21,92 +20,140 @@ KAFKA_BOOTSTRAP_SERVERS = os.environ.get("KAFKA_BOOTSTRAP_SERVERS")
 KAFKA_TOPIC = os.environ.get("KAFKA_TOPIC")
 
 API_KEY = os.environ.get("OPENWEATHER_API_KEY")
-LAT = os.environ.get("LAT")
-LON = os.environ.get("LON")
+LOCATIONS_JSON = os.environ.get("LOCATIONS_JSON")
+LOCATIONS = json.loads(LOCATIONS_JSON) if LOCATIONS_JSON else []
 
-
-URL = f"https://api.openweathermap.org/data/3.0/onecall?lat={LAT}&lon={LON}&exclude=hourly,daily,current&units=metric&appid={API_KEY}"
 CALL_INTERVAL = 60  # seconds
+
+
+def build_url(lat, lon):
+    return f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=hourly,daily,current&units=metric&appid={API_KEY}"
 
 
 async def wait_for_kafka_connection(bootstrap_servers, retries=10, delay=2):
     """
     Asynchronously tries to establish a connection to Kafka with a retry mechanism.
+
+    This function attempts to create a Kafka producer connection to the specified 
+    bootstrap servers. It includes a retry mechanism to handle transient connection 
+    issues, logging each attempt and its outcome.
+
+    Parameters:
+    ----------
+    bootstrap_servers : str
+        A comma-separated string of Kafka bootstrap server addresses. 
+        Example: "localhost:9092,localhost:9094".
+
+    retries : int, optional
+        The maximum number of attempts to connect to Kafka. Default is 10.
+
+    delay : int, optional
+        The number of seconds to wait between connection attempts. Default is 2 seconds.
+
+    Returns:
+    -------
+    KafkaProducer
+        An instance of KafkaProducer if the connection is successful.
+
+    Raises:
+    -------
+    Exception
+        Raises an exception if a connection to Kafka cannot be established 
+        after the specified number of retries.
     """
     for attempt in range(retries):
         try:
             # Initialize Kafka producer
             producer = KafkaProducer(
-                bootstrap_servers=bootstrap_servers,
-                key_serializer=lambda k: k.encode("utf-8") if isinstance(k, str) else k,
-                value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-                compression_type=os.environ.get("KAFKA_PRODUCER_COMPRESSION_TYPE"),
-                retries=int(
-                    os.environ.get("KAFKA_PRODUCER_RETRIES")
-                ),  # Retry up to X times on failure
-                batch_size=128,
-                linger_ms=50,
-                acks=os.environ.get("KAFKA_PRODUCER_ACKS"),
-                request_timeout_ms=30000,  # Set timeout to 30 seconds
-                metadata_max_age_ms=60000,  # Allow 60 seconds to refresh metadata
+                bootstrap_servers=bootstrap_servers, # Kafka broker addresses
+                key_serializer=lambda k: k.encode("utf-8") if isinstance(k, str) else k, # Serialize function. Converts string keys to UTF-8 encode bytes.
+                value_serializer=lambda v: json.dumps(v).encode("utf-8"), # Serialize function. Converts the value to a JSON string and then encodes it as UTF-8 bytes.
+                retries=5,  # Sets the number of retry attempts the producer will make if sending a message fails.
+                batch_size=16384, # Defines the maximum size (in bytes) of a batch of messages that the producer will send to Kafka in one request. (16 kB)
+                linger_ms=0, # Specifies the time (in milliseconds) to wait before sending a batch of messages. Setting to 0, messages will be sent immediately without waiting.
+                acks='all', # Defines the number of acknowledgments the producer requires from Kafka before considering a request complete.
+                request_timeout_ms=60000,  # Sets the maximum time (in milliseconds) the producer will wait for a response from Kafka before timing out. (60 sec)
+                metadata_max_age_ms=60000,  # Defines how long (in milliseconds) the producer should cache metadata about the brokers. (60 sec)
             )
             log.info(f"Kafka producer connection established on attempt {attempt + 1}")
             return producer
-        except NoBrokersAvailable:
+        except Exception as e:
             log.warning(
                 f"Kafka producer connection failed. Retrying in {delay} seconds... (Attempt {attempt + 1}/{retries})"
             )
+            log.error(f"Error: {e}")
             await asyncio.sleep(delay)
 
     raise Exception("Failed to connect to Kafka after multiple attempts.")
 
 
-def fetch_weather_data(producer):
+async def fetch_weather_data(producer):
     """
-    Fetches weather data from the OpenWeatherMap API and sends it to a Kafka topic.
+    Fetches weather data from the OpenWeatherMap API for specified locations and sends it to a Kafka topic.
+
+    This function iterates over predefined locations, constructs the API request URL for each location,
+    and retrieves weather data asynchronously. It extracts precipitation data from the response, calculates 
+    the total precipitation, and sends this information to the specified Kafka topic. The function also 
+    logs the status of the sent message and handles potential errors during the fetching and sending process.
+
+    Parameters:
+        producer (KafkaProducer): An instance of the KafkaProducer used to send messages to the Kafka topic.
+
+    Raises:
+        requests.exceptions.RequestException: If there is an error during the API request.
+
+    Notes:
+        - It waits for an acknowledgment from Kafka after sending each message and logs success or failure 
+          accordingly.
     """
-    try:
-        response = requests.get(URL)
-        response.raise_for_status()  # Raise an error for bad responses
-        data = response.json()
 
-        if "minutely" in data:
-            precip_data = data["minutely"]
-            total_precip = round(
-                sum([minute.get("precipitation", 0) for minute in precip_data]), 5
-            )
-            timestamp = precip_data[0]["dt"]
+    for location in LOCATIONS:
+        lat = location["lat"]
+        lon = location["lon"]
+        url = build_url(lat, lon)
 
-            # Send data to Kafka
-            record = producer.send(
-                KAFKA_TOPIC,
-                {"timestamp": timestamp, "total_precipitation": total_precip},
-            )
+        try:
+            response = await asyncio.to_thread(requests.get, url)
+            response.raise_for_status()  # Raise an error for bad responses
+            data = response.json()
 
-            # Wait for the Kafka acknowledgment
-            try:
-                record_metadata = record.get(timeout=30)  # Wait for acknoledge
-                log.info(
-                    f"Message sent successfully to topic {record_metadata.topic} partition {record_metadata.partition} offset {record_metadata.offset}. Sent data: {datetime.fromtimestamp(timestamp)} | Precipitation: {total_precip} mm"
+            if "minutely" in data:
+                precip_data = data["minutely"]
+                total_precip = round(
+                    sum([minute.get("precipitation", 0) for minute in precip_data]), 5
                 )
-            except Exception as e:
-                log.error(f"Failed to send message: {e}")
-        else:
-            log.info("No 'minutely' data available")
-    except requests.exceptions.RequestException as e:
-        log.error(f"Error fetching data: {e}")
+                timestamp = precip_data[0]["dt"]
+                # Send data to Kafka
+                record = producer.send(
+                    KAFKA_TOPIC,
+                    {
+                        "timestamp": timestamp,
+                        "total_precipitation": total_precip,
+                        "location": {"lat": lat, "lon": lon},
+                    },
+                )
+
+                # Wait for the Kafka acknowledgment
+                try:
+                    record_metadata = record.get(timeout=30)  # Wait for acknoledge
+                    log.info(
+                        f"Message sent successfully to topic {record_metadata.topic} partition {record_metadata.partition} offset {record_metadata.offset}. Sent data: {datetime.fromtimestamp(timestamp)} | Precipitation: {total_precip} mm | Latitude: {lat} | Longitude: {lon}"
+                    )
+                except Exception as e:
+                    log.error(f"Failed to send message: {e}")
+            else:
+                log.info("No 'minutely' data available for location {lat}, {lon}")
+        except requests.exceptions.RequestException as e:
+            log.error(f"Error fetching data for location {lat}, {lon}: {e}")
 
 
 async def main():
     producer = await wait_for_kafka_connection(KAFKA_BOOTSTRAP_SERVERS)
 
     while True:
-        fetch_weather_data(producer)
-        await asyncio.sleep(
-            CALL_INTERVAL
-        )  # Wait for 60 seconds before the next API call
+        await fetch_weather_data(producer)
+        await asyncio.sleep(CALL_INTERVAL)
 
-    # Close the connection when done
     producer.close()
 
 
